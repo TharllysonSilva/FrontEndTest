e1fcd2e9be909976c6b62169ed2fe6e567689e74
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-03-26 12:20:46 -0300

    Function UserListPage

--- src/pages/UserListPage.js
+++ src/pages/UserListPage.js
@@ -1,1 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+function UserListPage({ name, username, email, phone, id }) {
+  return <div></div>;
+}
+
+export default UserListPage;
